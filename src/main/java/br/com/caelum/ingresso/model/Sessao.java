4b4395947f7c5e47f2613a43c198693d880c71a5
wilian <wsilva@>
2017-08-05 12:11:21 -0300

    Criando a regra de negócio da sessão

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,77 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Sessao {
+
+	@Id  //gerando uma chave primaria
+	@GeneratedValue //deixando que o spring gerencie o auto-complete
+	private Integer id;
+	private LocalTime horario;
+	
+	@ManyToOne  //relacionamento muitos para um.
+	private Sala sala;
+	
+	@ManyToOne
+	private Filme filme;
+	
+	
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+	}
+	
+	public Sessao(LocalTime horario, Sala sala, Filme filme) {
+		this.horario = horario;
+		this.sala = sala;
+		this.filme = filme;
+	}
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {	
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	
+	public LocalTime getHorarioTermin(){
+		return this.horario.plus(filme.getDuracao().toMinutes(), ChronoUnit.MINUTES);
+	}
+	
+	
+	
+}
